ab5b9d7a1a47621aead35da8fecb341a9da79f73
Jhony <jhonyalkhoury8@gmail.com>
2023-10-28 21:47:33 -0600

    test add element

--- public/script.js
+++ public/script.js
@@ -35,7 +35,7 @@ firstRectangle.addEventListener('mouseup',(e)=>
     let newGreeting= document.createElement('div');
     newGreeting.class="greeting";
     newGreeting.id="HelloAgainJames!";
-    newGreeting.text="Hello again, James!";
+    newGreeting="Hello again, James!";
     bodys.appendChild(newGreeting);
     //console.log(e.clientX,e.clientY);
     //console.log("HERE");
