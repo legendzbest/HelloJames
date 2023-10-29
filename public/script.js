f4743bf1b0345bb67d7343b93ccbafe70d01a9be
Jhony <jhonyalkhoury8@gmail.com>
2023-10-28 20:33:14 -0600

    test drag

--- public/script.js
+++ public/script.js
@@ -1,13 +1,6 @@
 const firstRectangle=document.getElementById("HelloJames!");
-const counter=document.getElementById("Counter");
-let number=0;
-firstRectangle.addEventListener('dragstart',  drag)
-//firstRectangle.addEventListener('drop', drop);
-const drag = (e)=>{
-    
-    console.log(e);
-};
-const drop= (e)=>
+let offsetX,offsetY,Dragging=false;
+firstRectangle.addEventListener('dragstart',(e)=>
 {
     console.log(e);
-};
+});
