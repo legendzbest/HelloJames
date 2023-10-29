80007a63cf721dda356f620eb1c6d0409a533abc
Jhony <jhonyalkhoury8@gmail.com>
2023-10-28 21:38:07 -0600

    test add element

--- public/script.js
+++ public/script.js
@@ -31,11 +31,12 @@ firstRectangle.addEventListener('mousemove',(e)=>
 );
 firstRectangle.addEventListener('mouseup',(e)=>
 {
-    Dragging=false;
     let newGreeting= document.createElement('div');
     //newGreeting.class="greeting";
     //newcontent.id="HelloAgainJames!";
     bodys.appendchild(newGreeting);
     //console.log(e.clientX,e.clientY);
+    Dragging=false;
+
 }
 );
