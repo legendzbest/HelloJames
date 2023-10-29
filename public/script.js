8e6c4cf2cc7e948d0082b526667bd3846cbbb0a1
Jhony <jhonyalkhoury8@gmail.com>
2023-10-28 21:03:01 -0600

    test drag

--- public/script.js
+++ public/script.js
@@ -14,8 +14,8 @@ firstRectangle.addEventListener('mousemove',(e)=>
     if(e.button==0)
     {   if(Dragging)
         {
-            firstRectangle.style.left=e.clientX;
-            firstRectangle.style.top=e.clientY;
+            firstRectangle.style.left=e.clientX+'px';
+            firstRectangle.style.top=e.clientY+'px';
         }
         //console.log(e);
     }
